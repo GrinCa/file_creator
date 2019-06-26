b1467433bcc48ce638bb2b6fc57a4e349233773a
Julien Ecobichon <julien9844.je@gmail.com>
2019-06-26 09:49:06 +0200

    quelques avancées dans la classe FIleCreator

--- src/file_creator/FileCreator.java
+++ src/file_creator/FileCreator.java
@@ -6,21 +6,56 @@
 package file_creator;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Julien
  */
 public class FileCreator {
-    
-    public FileCreator(String path){
+
+    public FileCreator(String path) {
         this.path = path;
         System.out.println(path);
     }
-    
-    public void createFile(){
-        
+
+    public void initializeFolder() {
+        this.createFolder(path);
+        this.createFolder(path + "\\src");
+        this.createFolder(path + "\\nbproject");
+        this.createFolder(path + "\\build");
+
+        this.createFolder(path + "\\src\\" + nomPackage);
     }
-    
+
+    public void initializeFile() {
+        this.createFile(path + "\\src\\" + nomPackage + "\\" + nomProjet + ".java");
+    }
+
+    private void createFolder(String file) {
+        new File(file).mkdir();
+    }
+
+    private void createFile(String path) {
+        try {
+            new File(path).createNewFile();
+        } catch (IOException ex) {
+            Logger.getLogger(FileCreator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void findTitle() {
+        for (int i = 0; i < path.length(); i++) {
+            if(path.substring(i) == "\\"){
+                
+            }
+        }
+    }
+
     private String path = "";
+    private String nomProjet = "project";
+    private String nomPackage = "main." + nomProjet;
+
 }
